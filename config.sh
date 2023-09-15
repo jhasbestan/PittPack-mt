56bb09fb575e5b2f08f25cee494b83759ae1c619
jhasbestan <jaber.hasbestan@amd.com>
2023-09-15 16:04:02 -0500

    minor

--- config.sh
+++ config.sh
@@ -8,6 +8,6 @@ module load aocl-4.0
 module load ompi-aocc
 
 
-cd build && cmake .. -DCMAKE_BUILD_TYPE=release -DENABLE_THREAD=true -DNO_HDF5=true && make -j 8 && cd ../
+cd build && cmake .. -DCMAKE_BUILD_TYPE=release -DENABLE_THREAD=true -DNO_HDF5=true && make -j 8 && make install && cd ../
 
 
