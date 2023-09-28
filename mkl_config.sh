cf951edd37e34a72093107bb37a286e848522c09
jhasbestan <jaber.hasbestan@amd.com>
2023-09-28 15:02:35 -0500

    generalized to swap math lib easily

--- mkl_config.sh
+++ mkl_config.sh
@@ -1,11 +1,12 @@
 #! /usr/bin/bash
 
 
-
-module load icc/latest
-module load impi/latest
+module purge
+module load aocc-4.0
+module load ompi-aocc
+module load hdf5-aocc-ompi
 module load mkl/latest
 
-cd build && cmake .. -DCMAKE_BUILD_TYPE=release -DNO_HDF5=true && make -j 8 && make install && cd ../
+cd build && cmake .. -DCMAKE_BUILD_TYPE=release -DNO_HDF5=true  -DMKL=true && make -j 8 && make install && cd ../
 
 
